472b053622ce1b6ec83a8c4b13e336d4cb1984c5
Rico Roma <ricoroma14@gmail.com>
2020-07-10 11:13:01 -0300

    Favicon

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <title>@yield('title')</title>
