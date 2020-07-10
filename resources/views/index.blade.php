a3780cef0995fc5e2957daef1785554e1aa8f0a6
Rico Roma <ricoroma14@gmail.com>
2020-07-10 12:00:38 -0300

    View index Row apresentação

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,21 @@
 @section('title', 'Minha Estante - Index')
 @section('content')
 
+<div class="row w-100 text-center ml-auto mr-auto mb-5 border border-dark" style="height: 50px;" id="presentRow">
+    <div class="col-sm p-1 h-100">
+        <img class="h-100 mb-1" src="{{ asset('img/computer.png') }}" alt="...">
+        <h5 class="text-dark d-inline-block p-2 font-weight-bold font-italic">Consulta rápida</h5>
+    </div>
+    <div class="col-sm p-1 h-100">
+        <img class="h-100 mb-1" src="{{ asset('img/book-keeper.png') }}" alt="...">
+        <h5 class="text-dark d-inline-block p-2 font-weight-bold font-italic">Atendimento personalizado</h5>
+    </div>
+    <div class="col-sm p-1 h-100">
+        <img class="h-100 mb-1" src="{{ asset('img/return-cart.png') }}" alt="..." />
+        <h5 class="text-dark d-inline-block p-2 font-weight-bold font-italic">Facilidade no pagamento</h5>
+    </div>
+</div>
+
 <div class="container">
 
     <div class="row"  style="height: 450px;">
@@ -16,12 +31,12 @@
         </div>
         <div class="col-sm h-100">
             
-            <p class="title">Novos Livros</p>
+            <p class="title">Nossos livros</p>
             
             <div class="card border-dark position-relative" id="cardCadastroLivro">
                 <img class="card-img-top w-100" src="{{ asset('img/bg-image.jpg') }}" alt="ImgCadLivro">
                 <div class="card-body w-100">
-                    <p class="card-text">Registre seu livro favorito em nossa base de dados e compartilhe sua leitura conosco!</p>
+                    <p class="card-text">Consulte nossa grande biblioteca de livros disponíveis na loja</p>
                     <a href="#" class="btn btn-primary">Ir</a>
                 </div>
             </div>
