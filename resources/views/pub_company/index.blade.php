4dc0d04325e9f150e55499c20ed9ae6b8c4e3c89
Rico Roma <ricoroma14@gmail.com>
2020-07-13 23:10:53 -0300

    View dash_pubs search-toggle

--- resources/views/pub_company/index.blade.php
+++ resources/views/pub_company/index.blade.php
@@ -3,7 +3,17 @@
 @section('title', 'Editoras')
 @section('content')
 
-<div id="screen" class="open">
+<div id="screen" class="close ml-auto">
+
+    <div class="row m-0 w-100">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark w-100">        
+                {{--  search tool  --}}
+                <form class="form-inline my-2 my-lg-0 w-100">
+                    <input class="form-control col-sm-11" type="search" placeholder="Pesquisar" aria-label="Search">
+                    <button class="btn btn-primary col-sm-1" type="submit">Ir</button>
+                </form>
+        </nav>
+    </div>
 
 </div>
     
